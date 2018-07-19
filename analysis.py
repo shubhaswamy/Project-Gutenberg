cc797e329ec7c16ef6a6d40f7423108028f27a99
Shubha Swamy <shubhaswamy2@gmail.com>
2018-07-19 15:08:16 -0700

    initialized methods

---
+++ analysis.py
@@ -0,0 +1,26 @@
+class TextAnalysis():
+    # Rocket simulates a rocket ship for a game,
+    #  or a physics simulation.
+    
+    def __init__(self):
+    	pass
+        
+    def getTotalNumberOfWords(self):
+    	pass
+
+    def getTotalUniqueWords(self):
+    	pass
+
+    def get20MostFrequentWords(self):
+    	pass
+
+    def get20MostInterestingFrequentWords(self):
+    	pass
+
+    def get20LeastFrequentWords(self):
+    	pass
+
+     
+
+
+        
